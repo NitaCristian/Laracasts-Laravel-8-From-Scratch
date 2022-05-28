ef8845494ba2d50c8673d76eb7d582a1bb36cd41
Cristian Nita <cristiannita24@gmail.com>
2022-05-28 17:51:48 +0300

    Create a new Post class
    The Post class contains all the logic necessary to fetch posts.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,35 +4,9 @@
 <link rel="stylesheet" href="/css/app.css">
 
 <body>
-<article>
-    <h1><a href="/post/my-first-post">My First Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet aperiam debitis doloribus, eos eum expedita
-        illo ipsum itaque maiores molestiae nulla, perspiciatis placeat quasi quidem recusandae reiciendis, sequi sint
-        soluta ullam. Ab commodi cumque debitis deserunt, distinctio doloremque dolorum ea eligendi enim eos et eveniet
-        expedita facere fugiat in iure laboriosam magnam nesciunt nihil pariatur quaerat qui repudiandae sapiente sed ut
-        veritatis vero vitae voluptas voluptates voluptatum! Nesciunt, obcaecati.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-second-post">My Second Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam asperiores assumenda atque nisi possimus
-        quo recusandae reprehenderit! Cupiditate dignissimos dolores fugiat harum id, nam necessitatibus officia quia
-        quidem repellat soluta ullam veritatis voluptas. Accusantium nihil optio quasi sit soluta. Eaque eius facilis
-        iste libero pariatur quidem, quos ratione unde.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-third-post">My Third Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet aperiam debitis doloribus, eos eum expedita
-        illo ipsum itaque maiores molestiae nulla, perspiciatis placeat quasi quidem recusandae reiciendis, sequi sint
-        soluta ullam. Ab commodi cumque debitis deserunt, distinctio doloremque dolorum ea eligendi enim eos et eveniet
-        expedita facere fugiat in iure laboriosam magnam nesciunt nihil pariatur quaerat qui repudiandae sapiente sed ut
-        veritatis vero vitae voluptas voluptates voluptatum! Nesciunt, obcaecati.
-    </p>
-</article>
+@foreach($posts as $post)
+    <article>
+        {!! $post !!}
+    </article>
+@endforeach
 </body>
