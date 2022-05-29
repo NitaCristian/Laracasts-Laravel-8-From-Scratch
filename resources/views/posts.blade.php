4bd6d1f000090ca1663f2970b10ce4107a51fd2b
Cristian Nita <cristiannita24@gmail.com>
2022-05-29 12:15:19 +0300

    Blade Basics
    First type of layout files using Blade directives.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,17 @@
-<!doctype html>
+@extends('layout')
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/css/app.css">
+@section('content')
+    @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/post/{{$post->slug}}">
+                    {{ $post->title }}
+                </a>
+            </h1>
 
-<body>
-@foreach($posts as $post)
-    <article>
-        <h1>
-            <a href="/post/{{$post->slug}}">{{$post->title}}</a>
-        </h1>
-
-        <div>
-            {!! $post->excerpt !!}
-        </div>
-    </article>
-@endforeach
-</body>
+            <div>
+                {!! $post->excerpt !!}
+            </div>
+        </article>
+    @endforeach
+@endsection
